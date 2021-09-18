9847434bc8b193abf136ce90f47b9af056fb705b
Glenn Turner <glenn@standalone.com>
2021-09-18 09:08:46 -0500

    Tweaked template display.

--- template-parts/content.php
+++ template-parts/content.php
@@ -16,7 +16,16 @@
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif; ?>
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				mediaclature_posted_on();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<?php mediaclature_post_thumbnail(); ?>
