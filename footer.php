7dd8e532c7a5b3dcf8d45e46795ab7417bd87530
Glenn Turner <glenn@standalone.com>
2021-09-18 22:11:44 -0500

    Added author name display.

--- footer.php
+++ footer.php
@@ -13,6 +13,19 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
+			<?php
+			/* Super dumb way to get the main author, but it's fine for now. */
+			$user = get_user_by( 'id', 1 );
+
+			if ( $user ):
+			?>
+			<span class="author-info">
+				Authored by <a href="<?php esc_html_e( $user->url ); ?>" rel="external"><?php
+
+					esc_html_e( $user->first_name . ' ' . $user->last_name );
+				?></a>
+			</span>
+			<?php endif; ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
