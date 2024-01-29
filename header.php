c36394c53bba8b1264fb57d14cc891f6d879c975
Glenn Turner <glenn@standalone.com>
2024-01-29 11:25:21 -0600

    Replaced hamburger menu with standard primary menu.

--- header.php
+++ header.php
@@ -32,6 +32,7 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<div
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'mediaclature' ); ?></a>
 
@@ -58,10 +59,14 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
+			<?php
+				/*
 			<button class="menu-toggle primary-mobile-menu" aria-controls="primary-menu" aria-expanded="false">
 				<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
 					<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
 				</svg>
+				*/
+			?>
 				<?php
 					// esc_html_e( 'Primary Menu', 'mediaclature' );
 				?>
