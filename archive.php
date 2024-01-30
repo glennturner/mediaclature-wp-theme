5eda738e10d7cc603acd2c3b582fa09bed1342e8
Glenn Turner <glenn@standalone.com>
2024-01-30 17:29:49 -0600

    Added newsletter block.

--- archive.php
+++ archive.php
@@ -28,6 +28,7 @@ get_sidebar();
 
 			<?php
 			/* Start the Loop */
+			$i = 0;
 			while ( have_posts() ) :
 				the_post();
 
@@ -36,13 +37,14 @@ get_sidebar();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
+				get_template_part( 'template-parts/content', get_post_type(), array( 'i' => $i ) );
+				$i += 1;
 			endwhile;
 
 			the_posts_navigation();
 
-		else :
+
+		else:
 
 			get_template_part( 'template-parts/content', 'none' );
 
